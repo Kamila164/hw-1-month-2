bccb977e01805f82e4ac7c25d0b28dcf2a4551aa
kamila <murtazaeva.kamila01@gmail.com>
2022-11-19 15:46:46 +0600

    edit

--- hw-1-month-2/main.swift
+++ hw-1-month-2/main.swift
@@ -1,11 +1,34 @@
-//
-//  main.swift
-//  hw-1-month-2
-//
-//  Created by Kamila on 17/11/22.
-//
-
 import Foundation
 
-print("Hello, World!")
 
+class Car{
+    var model: String
+    var year: Int
+    var maxSpeed: Int
+    var power : Int
+    var accelerationTime: Double
+
+    init(model: String, year: Int, maxSpeed: Int, power: Int, accelerationTime: Double) {
+        self.model = model
+        self.year = year
+        self.maxSpeed = maxSpeed
+        self.power = power
+        self.accelerationTime = accelerationTime
+    }
+
+    func getTuning(){
+        let randomNumber = Int.random(in: 1...3)
+        if randomNumber == 2{
+            let newSpeed = maxSpeed + Int.random(in: 10...100)
+            let newPower = power + Int.random(in: 10...100)
+            let newAcceleration = accelerationTime - Double.random(in: 0.1...0.9)
+            print("Модель машины: \(model) \nГод: \(year) \nМакс. скорость: \(newSpeed) \nМощность: \(newPower) \nВремя разгона: \(newAcceleration)")
+        }else{
+            print("Тюнинг не удался")
+        }
+    }
+}
+
+var car: Car = Car(model: "BMW X6", year: 2015, maxSpeed: 236, power: 245, accelerationTime: 7.5)
+
+car.getTuning()
